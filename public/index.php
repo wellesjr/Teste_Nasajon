19ebc5583f7c993c49184095a566b9e629ab7c22
Welles Jr <welles-junior@hotmail.com>
2026-01-15 18:25:27 -0300

    feat: criado public

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+require __DIR__ . '/../app/Support/Config.php';
+require __DIR__ . '/../app/Support/HttpClient.php';
+require __DIR__ . '/../app/Support/Normalizer.php';
+
+require __DIR__ . '/../app/Models/MunicipioIbge.php';
+require __DIR__ . '/../app/Models/ResultadoLinha.php';
+
+require __DIR__ . '/../app/Services/CsvService.php';
+require __DIR__ . '/../app/Services/IbgeService.php';
+require __DIR__ . '/../app/Services/MatcherService.php';
+require __DIR__ . '/../app/Services/StatsService.php';
+require __DIR__ . '/../app/Services/SubmitService.php';
+
+require __DIR__ . '/../app/Controllers/IbgeController.php';
+
+use App\Controllers\IbgeController;
+
+$controller = new IbgeController();
+$controller->run($argv);
